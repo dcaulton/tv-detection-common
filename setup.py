7ec7fe3aaf0bdf945c2bf1456707a00ec919fadc
Dave Caulton <dcaulton@gmail.com>
2026-01-18 10:49:49 -0600

    update basic structure for future flexibility

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='tv-detection-common',
     version='0.1.0',
-    packages=['tv_detection_common'],
-    install_requires=['sqlalchemy>=1.4'],
+    packages=find_packages(),
+    install_requires=['sqlalchemy'],
     description='Shared DB models for tv-detection projects'
 )
